8851a4cae73d5661cbc62cc815ce218dfc031e39
Erik Sidnei Corrêa <erik562@hotmail.com>
2017-04-04 21:59:54 -0300

    Melhorias no codigo

--- js/teste.js
+++ js/teste.js
@@ -1,13 +1,22 @@
 function somaCalorias(){
-var produto =document.getElementsByTagName("input");
-var qtdProdutos = produto.length;
-var total= 0;
 
-for (i= 0; i < qtdProdutos; i++){
-    if(produto[i].checked == true){
-        total= total + parseInt(produto[i].value);
-    } 
-}
-    $("#totalcal").text(total);
+
 };
+var btnCalcular = document.querySelector(".btnCalcular");
+btnCalcular.addEventListener("click",function (envent){
+    event.preventDefault();
+    
+    var produto = document.querySelectorAll("input");
+    var qtdProdutos = produto.length;
+    var total= 0;
 
+    for (i= 0; i < qtdProdutos; i++){
+        if(produto[i].checked == true){
+            total= total + parseInt(produto[i].value);
+        } 
+    }
+    
+    var totalcal = document.querySelector("#totalcal");
+    totalcal.textContent = total;
+
+})
